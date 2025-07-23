5350f8d64a1d2bdcdf2cc6250c877ca4ae90f59f
prangel-git <62568827+prangel-git@users.noreply.github.com>
2025-07-23 10:26:14 -0400

    adding is_square function

--- numbertheory/src/chapter2.py
+++ numbertheory/src/chapter2.py
@@ -1,11 +1,19 @@
 from math import isqrt
 
 
+def is_square(pyramid_number):
+    sqrt_pyramid = isqrt(pyramid_number)
+    is_square_number = sqrt_pyramid * sqrt_pyramid == pyramid_number
+    return is_square_number
+
+
 def exercise2_12(bound=1000):
     for a in range(2, bound):
         pyramid_number = a * (a + 1) * (2 * a + 1) // 6
-        sqrt_pyramid = isqrt(pyramid_number)
-        is_square_number = sqrt_pyramid * sqrt_pyramid == pyramid_number
-        if is_square_number:
+        if is_square(pyramid_number):
             return pyramid_number
+    return 0
+
+
+def exercise2_13(bound=1000):
     return 0
--- numbertheory/tests/test_chapter2.py
+++ numbertheory/tests/test_chapter2.py
@@ -1,5 +1,10 @@
 from numbertheory.src.chapter2 import *
 
 
+def test_is_square():
+    assert is_square(2) == False
+    assert is_square(9) == True
+
+
 def test_exercise2_12():
     assert exercise2_12() == 4900
